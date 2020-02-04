672f6051a81294411af20b41a20a6e974ec40a85
Georgiy <agafon13@gmail.com>
2020-02-04 06:10:40 +0300

    try login with unittst

--- login.py
+++ login.py
@@ -11,3 +11,5 @@ browser.implicitly_wait(10)
 browser.find_element_by_id("email").send_keys("seleniumisgood@mail.com")
 browser.find_element_by_id("passwd").send_keys("123456")
 browser.find_element_by_id("SubmitLogin").click()
+assert "My account - My Store" in browser.title
+browser.close()
---
+++ login_new.py
@@ -0,0 +1,26 @@
+import unittest
+from selenium import webdriver
+#from selenium.webdriver.common.keys import Keys
+
+class login_automationpractice(unittest.TestCase):
+    
+    def setUp (self):
+        self.browser = webdriver.Chrome()
+    
+    def login_ap (self):
+        browser = self.browser
+        browser.get("http://automationpractice.com/index.php")
+        self.assertIn("My Store", browser.title)
+        browser.find_element_by_class_name("login").click()
+        browser.implicitly_wait(10)
+        browser.find_element_by_id("email").send_keys("seleniumisgood@mail.com")
+        browser.find_element_by_id("passwd").send_keys("123456")
+        browser.find_element_by_id("SubmitLogin").click()
+        assert "My account - My Store" in browser.title
+    
+    def tearDown(self):
+        self.browser.close()
+    
+if __name__ == "__main__":
+    unittest.main()
+    
